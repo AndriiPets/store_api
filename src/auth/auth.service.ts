b4b1ce2aea8c07fb0d486ac044072d32b4724b83
AndriiPets <nnggtrgftgd@gmail.com>
2022-12-24 01:58:12 +0200

    initial auth implimentation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt/dist';
+import { ConfigService } from '@nestjs/config/dist/config.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  signin() {
-    return { msg: 'i have sign up!' };
+  async signin(dto: AuthDto) {
+    //find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //if user dose not exist throw exeption
+    if (!user) throw new ForbiddenException('Credentials incorect!');
+
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    //if password incorectthrow exeption
+    if (!pwMatches) throw new ForbiddenException('Credentials incorect!');
+
+    //return user
+    return this.signToken(user.id, user.email);
   }
 
   async signup(dto: AuthDto) {
-    //generate password hash
-    const hash = await argon.hash(dto.password);
-    //save new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
+    try {
+      //generate password hash
+      const hash = await argon.hash(dto.password);
+      //save new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      //return saved user
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('invalid credentials');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '30m',
+      secret: secret,
     });
-    //return saved user
-    return user;
+
+    return {
+      access_token: token,
+    };
   }
 }
